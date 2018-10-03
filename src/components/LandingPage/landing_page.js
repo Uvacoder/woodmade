95b7182d70d946acd771b29a03e412c7bcc18650
Brandon Shapiro <shapyro@Brandons-MacBook-Pro.local>
2018-10-03 16:40:41 -0500

    landing page

---
+++ src/components/LandingPage/landing_page.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import backGrnd from '../../images/heartmade-landing(1).png';
+import './landing_page.css';
+
+// const backStyle = {
+//   backgroundImage: `url(${backGrnd})`
+// };
+
+class Landing extends Component {
+  render() {
+    // return <div style={backStyle}>HEARTMADE</div>;
+    return <div>HEARTMADE</div>;
+  }
+}
+
+export default Landing;
